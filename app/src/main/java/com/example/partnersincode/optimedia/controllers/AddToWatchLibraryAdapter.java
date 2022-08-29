e460bf4d296dc80ecc9bb5752d500aa8feed0992
MakkuSaiko <AdriaanBenn0@gmail.com>
2022-08-29 10:03:23 +0200

    Started implementing UI logic for A02310

---
+++ app/src/main/java/com/example/partnersincode/optimedia/controllers/AddToWatchLibraryAdapter.java
@@ -0,0 +1,18 @@
+package com.example.partnersincode.optimedia.controllers;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AddToWatchLibraryAdapter extends RecyclerView.ViewHolder
+{
+    private View.OnClickListener onClickListener;
+
+//    public static class
+
+
+    public AddToWatchLibraryAdapter(@NonNull View itemView) {
+        super(itemView);
+    }
+}
