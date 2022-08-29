bddcabfdeccd5dfdfdce25b7a629c5770b0ed0cf
MakkuSaiko <AdriaanBenn0@gmail.com>
2022-08-29 08:40:19 +0200

    UI set up for use case

---
+++ app/src/main/java/com/example/partnersincode/optimedia/ui/AddToWatchLibrary.java
@@ -0,0 +1,14 @@
+package com.example.partnersincode.optimedia.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddToWatchLibrary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_watch_library);
+    }
+}
