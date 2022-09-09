68822b906f5bbbc238e5cd67ae26137d541c0fe4
MakkuSaiko <AdriaanBenn0@gmail.com>
2022-09-09 15:20:11 +0200

    Most functionality implemented, just have to finish implementing the code that spans the XML over multiple QR codes. QR code level 18 used

--- app/src/main/java/com/example/partnersincode/optimedia/ui/shareLibrary/ShareLibrary.java
+++ app/src/main/java/com/example/partnersincode/optimedia/ui/shareLibrary/ShareLibrary.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.partnersincode.optimedia.R;
 
@@ -21,7 +22,24 @@ public class ShareLibrary extends Fragment {
         Button btnQR = view.findViewById(R.id.btnGenQR);
         btnQR.setOnClickListener(v->
         {
-           // Navigation.findNavController(view).navigate(R.id.); takes to generate qr code
+            //Create bundle to send to QR code fragment. TODO: Pass the XML as the correct datatype
+            Bundle bundle = new Bundle();
+            //Test string
+            bundle.putString("XML","8ZtVYoSPj8ZAceKpl9hE" +
+                            "UM5ojHm852EnwBnVmD7f" +
+                            "Qc0Mj9VEVYwlP4WzsmIA" +
+                            "yVYo8sBGpwBi5NQWtueQ" +
+                            "pVDD9diwIHE2X0GzoPer" +
+                            "vv2tvMAJdEAZKBzH5WHi" +
+                            "OHkAUQgtbr3BsclIuIl4" +
+                            "BzUG3ql2FrKRmkeGx1Za" +
+                            "UCvaaKo9tK7Ve6ZkaZxM" +
+                            "bn4vbeLtzQtKWpdZEbHJ"
+
+                    );
+
+            Navigation.findNavController(view).navigate(R.id.nav_showQrCode,bundle); //takes to generate qr code
+
         });
 
         Button btnXML = view.findViewById(R.id.btnCreateXML);
