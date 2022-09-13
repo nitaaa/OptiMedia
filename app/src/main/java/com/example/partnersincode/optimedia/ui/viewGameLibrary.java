5d2f3ef5987d217c0bec1adaa8b34d6b34a9078d
Qaanita Fataar <q.fataar22@gmail.com>
2022-09-14 00:12:56 +0200

    Added View Game A02200
    Still need game adapter, not fully working yet

--- app/src/main/java/com/example/partnersincode/optimedia/ui/viewGameLibrary.java
+++ app/src/main/java/com/example/partnersincode/optimedia/ui/viewGameLibrary.java
@@ -18,31 +18,31 @@ import android.widget.Toast;
 import com.example.partnersincode.optimedia.DatabaseHandler;
 import com.example.partnersincode.optimedia.R;
 import com.example.partnersincode.optimedia.adapters.BookAdapter;
-import com.example.partnersincode.optimedia.adapters.LibraryAdapter;
 import com.example.partnersincode.optimedia.models.Book;
+import com.example.partnersincode.optimedia.models.Game;
 import com.example.partnersincode.optimedia.models.Library;
 
 import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link viewBookLibrary#newInstance} factory method to
+ * Use the {@link viewGameLibrary#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class viewBookLibrary extends Fragment {
+public class viewGameLibrary extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private static final String TAG = "ViewBookLibrary:";
+    private static final String TAG = "ViewGameLibrary - ";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     private Library library;
 
-    public viewBookLibrary() {
+    public viewGameLibrary() {
         // Required empty public constructor
     }
 
@@ -52,11 +52,11 @@ public class viewBookLibrary extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment viewBookLibrary.
+     * @return A new instance of fragment viewGameLibrary.
      */
     // TODO: Rename and change types and number of parameters
-    public static viewBookLibrary newInstance(String param1, String param2) {
-        viewBookLibrary fragment = new viewBookLibrary();
+    public static viewGameLibrary newInstance(String param1, String param2) {
+        viewGameLibrary fragment = new viewGameLibrary();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -77,38 +77,39 @@ public class viewBookLibrary extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_view_book_library, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_view_game_library, container, false);
         DatabaseHandler dbHandler = new DatabaseHandler(this.getContext());
-        List<Book> bookList = dbHandler.getAllBooksLibrary(library.getLibraryID(),"");
-        RecyclerView reBookLibrary = rootView.findViewById(R.id.recyclerBookLibrary);
+        List<Game> gameList = dbHandler.getAllGamesLibrary(library.getLibraryID(),"");
+        RecyclerView reGameLibrary = rootView.findViewById(R.id.recyclerGameLibrary);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-        reBookLibrary.setLayoutManager(layoutManager);
-        BookAdapter adapter = new BookAdapter(bookList);
-        reBookLibrary.setAdapter(adapter);
-
-        TextView txtBookLibraryName = rootView.findViewById(R.id.txtBookLibraryName);
-        txtBookLibraryName.setText(library.getLibraryName());
-
-        adapter.setOnClickListener( view -> {
-            BookAdapter.BookViewHolder viewHolder = (BookAdapter.BookViewHolder) reBookLibrary.findContainingViewHolder(view);
-            Book book = viewHolder.book;
-            Toast.makeText(this.getContext(), book.toString(),Toast.LENGTH_LONG).show();
-            Bundle bundle = new Bundle();
-            bundle.putParcelable("bookInfo", book);
-     // TODO:Navigation from view book library to A02111
-            //  Navigation.findNavController(view).navigate(R.id., bundle); //navigate to A02111
-        });
+        reGameLibrary.setLayoutManager(layoutManager);
+//        GameAdapter adapter = new GameAdapter(gameList);
+//        reGameLibrary.setAdapter(adapter);
+//
+//        TextView txtGameLibraryName = rootView.findViewById(R.id.txtGameLibraryName);
+//        txtGameLibraryName.setText(library.getLibraryName());
+//
+//        adapter.setOnClickListener( view -> {
+//            GameAdapter.GameViewHolder viewHolder = (GameAdapter.GameViewHolder) reGameLibrary.findContainingViewHolder(view);
+//            Game game = viewHolder.game;
+//            Toast.makeText(this.getContext(), game.toString(),Toast.LENGTH_LONG).show();
+//            Bundle bundle = new Bundle();
+//            bundle.putParcelable("gameInfo", game);
+//            // TODO:Navigation from view book library to A02211
+//            //  Navigation.findNavController(view).navigate(R.id., bundle); //navigate to A02111
+//        });
 
         //TODO: onclick for edit
 
         //TODO: onclick for add to library - open search for books
 
         //onclick for share
-        Button imgBtnShareBLib = rootView.findViewById(R.id.imgBtnShareBLib);
-        imgBtnShareBLib.setOnClickListener(view -> {
+        Button imgBtnShareGLib = rootView.findViewById(R.id.imgBtnShareBLib);
+        imgBtnShareGLib.setOnClickListener(view -> {
             Bundle bundle = new Bundle();
-            bundle.putParcelable("BookLibraryInfo", library);
+            bundle.putParcelable("GameLibraryInfo", library);
             Navigation.findNavController(view).navigate(R.id.nav_shareLibrary, bundle);
         });
         return rootView;
